9bbea31069bd74f043fe05af0bd3e66fb206b7e0
sdpercy <scottpercy@hotmail.com>
2021-06-22 12:26:34 -0230

    added manager object & test to verify creattion

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,7 @@
+const Manager = require ('../lib/Manager');
+
+test('create a Manager object', () => {
+    const manager = new Manager('Scott', 4524, 'scottpercy@hotmail.com', 34);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
